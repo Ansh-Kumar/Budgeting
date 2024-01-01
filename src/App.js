9e3cab5fc3d6c62d2558b8116800eaac6624b10c
Ansh Kumar <ansh.khalasi@gmail.com>
2024-01-01 14:10:01 -0800

    created a basic log button

--- src/App.js
+++ src/App.js
@@ -1,54 +1,58 @@
 import React, { useState, useEffect } from "react";
-import Expenses from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpenses/NewExpense";
+// import Expenses from "./components/Expenses/Expenses";
+// import NewExpense from "./components/NewExpenses/NewExpense";
+import AddLog from "./components/Log/AddLog";
 
-// const DUMMY_EXPENSES = [];
+const DUMMY_EXPENSES = [];
 
-const DUMMY_EXPENSES = [
-	{
-		id: "e1",
-		title: "Toilet Paper",
-		amount: 94.12,
-		date: new Date(2020, 7, 14)
-	},
-	{
-		id: "e2",
-		title: "New TV",
-		amount: 799.49,
-		date: new Date(2021, 2, 12)
-	},
-	{
-		id: "e3",
-		title: "Car Insurance",
-		amount: 294.67,
-		date: new Date(2021, 2, 28)
-	},
-	{
-		id: "e4",
-		title: "New Desk (Wooden)",
-		amount: 450,
-		date: new Date(2021, 5, 12)
-	}
-];
+// const DUMMY_EXPENSES = [
+// 	{
+// 		id: "e1",
+// 		title: "Toilet Paper",
+// 		amount: 94.12,
+// 		date: new Date(2020, 7, 14)
+// 	},
+// 	{
+// 		id: "e2",
+// 		title: "New TV",
+// 		amount: 799.49,
+// 		date: new Date(2021, 2, 12)
+// 	},
+// 	{
+// 		id: "e3",
+// 		title: "Car Insurance",
+// 		amount: 294.67,
+// 		date: new Date(2021, 2, 28)
+// 	},
+// 	{
+// 		id: "e4",
+// 		title: "New Desk (Wooden)",
+// 		amount: 450,
+// 		date: new Date(2021, 5, 12)
+// 	}
+// ];
 
 function App() {
-	const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+	// const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
 	useEffect(() => {
-		document.title = "Budget Software";
+		document.title = "Track Every Penny";
 	}, []);
 
-	function addExpenseHandler(expense) {
-		console.log(expense);
-		setExpenses((prevExpenses) => {
-			return [expense, ...prevExpenses];
-		});
-	}
+	// function addExpenseHandler(expense) {
+	// 	console.log(expense);
+	// 	setExpenses((prevExpenses) => {
+	// 		return [expense, ...prevExpenses];
+	// 	});
+	// }
 
 	return (
+		// <div>
+		// 	<NewExpense onAddExpense={addExpenseHandler} />
+		// 	<Expenses items={expenses} />
+		// </div>
 		<div>
-			<NewExpense onAddExpense={addExpenseHandler} />
-			<Expenses items={expenses} />
+			<AddLog />
 		</div>
 	);
 }
